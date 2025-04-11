c035c497911e598a6141f10743890e543088c011
eduardescola <edduardescola@gmail.com>
2025-04-11 16:10:08 +0200

    funciona

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from "react"
+"use client"
+
+import { useEffect } from "react"
 import { Routes, Route, Navigate } from "react-router-dom"
 import Home from "./Home"
 import PokemonDetail from "./components/PokemonDetail"
 import EditPokemon from "./components/EditPokemon"
 import AddPokemon from "./components/AddPokemon"
 
-const App: React.FC = () => {
+export default function App() {
+  // Este useEffect eliminará el cursor de Mickey Mouse y hará el cursor de pokeball más pequeño
+  useEffect(() => {
+    // Crear un estilo que sobrescriba el cursor de Mickey Mouse con un cursor de pokeball más pequeño
+    const styleElement = document.createElement("style")
+    styleElement.innerHTML = `
+      html, body, *, *::before, *::after {
+        cursor: url("/public/poke32.png") 16 16, auto !important;
+      }
+    `
+    // Añadir el estilo al final del <head> para que tenga mayor prioridad
+    document.head.appendChild(styleElement)
+
+    return () => {
+      document.head.removeChild(styleElement)
+    }
+  }, [])
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -17,5 +36,3 @@ const App: React.FC = () => {
     </Routes>
   )
 }
-
-export default App
